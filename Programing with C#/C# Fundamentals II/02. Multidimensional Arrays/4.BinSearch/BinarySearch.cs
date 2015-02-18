32a9456a6c021f6e5622f3cf96d55caafc7937ee
Stanislav Kehayov <stenli327@gmail.com>
2015-02-18 22:06:35 +0200

    Add C# Fundamentals course

---
+++ Programing with C#/C# Fundamentals II/02. Multidimensional Arrays/4.BinSearch/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/// <summary>
+/// Write a program, that reads from the console an array of N integers and an integer K, sorts the array and using the method Array.BinSearch() finds the largest number in the array which is ≤ K.
+/// </summary>
+class BinarySearch
+{
+    static void Main()
+    {
+        Console.Write("Enter n number elements of aray to sort: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Console.Write("Enter K number for compare: ");
+        int k = int.Parse(Console.ReadLine());
+        Array.Sort(arr);
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+        int index = Array.BinarySearch(arr, k);
+
+        if (index < 0)
+        {
+            index = index * -1;
+        }
+        Console.WriteLine("Largest number in the array which is < or = of {0} is {1}", k, arr[index - 1]);
+
+    }
+}
---
+++ Programing with C#/C# Fundamentals II/02. Multidimensional Arrays/5.SortArrayByLenght/SortByString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// You are given an array of strings. Write a method that sorts the array by the length of its elements (the number of characters composing them).
+/// </summary>
+class SortByString
+{
+    static void Main()
+    {
+        string[] arr = { "gusto", "maina,", "ko staaa", "talasami", "kucheta", "opaa" };
+        SortByLenght(arr);
+        PrintMe(arr);
+    }
+
+    private static void PrintMe(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+
+    private static void SortByLenght(string[] arr)
+    {
+        Array.Sort(arr, (x, y) => x.Length.CompareTo(y.Length));
+    }
+}
